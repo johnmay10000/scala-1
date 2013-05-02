9ce8e2deb44588392e54a11978d09114185ff8a0
John May <johnmay10000@gmail.com>
2013-05-02 20:49:39 +1000

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "scala_1"
+
+version := "1.0"
+
+scalaVersion := "2.10.1"
---
+++ src/main/scala/HelloWorld.scala
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: johnmay
+ * Date: 2/05/13
+ * Time: 8:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+object HelloWorld extends App {
+  println("Hello, World!");
+}
